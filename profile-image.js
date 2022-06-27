60a8756cc11496c8b89a59a565e6343d8ef5279a
nkoepke <68548675+nkoepke@users.noreply.github.com>
2022-06-27 21:56:39 +0200

    Update profile-image.js

--- profile-image.js
+++ profile-image.js
@@ -38,7 +38,7 @@ class ProfileImage {
             hval ^= str.charCodeAt(i);
             hval += (hval << 1) + (hval << 4) + (hval << 7) + (hval << 8) + (hval << 24);
         }
-        if( asString ){
+        if(asString){
             // Convert to 8 digit hex string
             return ("0000000" + (hval >>> 0).toString(16)).substr(-8);
         }
