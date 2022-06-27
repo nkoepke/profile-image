927da436b945095be21690caa8a0a98644390dc9
nkoepke <68548675+nkoepke@users.noreply.github.com>
2022-06-27 22:42:18 +0200

    Update profile-image.js

--- profile-image.js
+++ profile-image.js
@@ -1,47 +1,42 @@
 class ProfileImage {
-    constructor(text = "NN", textColor = "#ffffff", backgroundColor = "#1081c1", font = "sans-serif", fontSize = 40) {
+    constructor(text = "NN", textColor = "#ffffff", backgroundColor = "#1081c1", font = "Arial", fontSize = 40) {
         this.text = text;
         this.textColor = textColor;
         this.backgroundColor = backgroundColor;
         this.font = font;
         this.fontSize = fontSize;
     }
-    svg(goodColor = false){
-        if(goodColor)
-            this.textColor = "#ffffff";
-        return `<svg viewBox='0 0 100 100' width='100' height='100' style='background-color:${(!goodColor ? this.backgroundColor : this.#getColor(this.text))}'><text x='50%' y='50%' alignment-baseline='central' text-anchor='middle' font-family='${this.font}' font-size='${this.fontSize}' fill='${this.textColor}' dominant-baseline='middle' text-anchor='middle'>${this.text}</text></svg>`;
+    svg(){
+        return `<svg viewBox='0 0 100 100' width='100' height='100' style='background-color:${this.backgroundColor}'><text x='50%' y='50%' alignment-baseline='central' text-anchor='middle' font-family='${this.font}' font-size='${this.fontSize}' fill='${this.textColor}' dominant-baseline='middle' text-anchor='middle'>${this.text}</text></svg>`;
     }
-    png(goodColor = false){
+    png(){
         let canvas = document.createElement("canvas");
         let context = canvas.getContext("2d");
         canvas.width = 256;
         canvas.height = 256;
-        // Draw background
-        context.fillStyle = (!goodColor ? this.backgroundColor : this.#getColor(this.text));
+        context.fillStyle = this.backgroundColor;
         context.fillRect(0, 0, canvas.width, canvas.height);
-        // Draw text
-        context.font = "bold 100px Arial";
+        context.font = "bold 100px " + this.font;
         context.fillStyle = this.textColor;
         context.textAlign = "center";
         context.textBaseline = "middle";
         context.fillText(this.text, canvas.width / 2, canvas.height / 2);
         return canvas.toDataURL("image/png");
     }
+    previewInConsole(){
+        console.log(`%c ${this.text} `, `background: ${this.backgroundColor}; color: ${this.textColor}`);
+    }
     #getArbitrary(seed, min, max) {
         return Math.floor(this.#hashFnv32a(seed, false, 7)/10000000000 * (max - min) + min);
     };
     #hashFnv32a(str, asString, seed) {
-        var i, l,
-            hval = (seed === undefined) ? 0x811c9dc5 : seed;
-    
+        var i, l, hval = (seed === undefined) ? 0x811c9dc5 : seed;
         for (i = 0, l = str.length; i < l; i++) {
             hval ^= str.charCodeAt(i);
             hval += (hval << 1) + (hval << 4) + (hval << 7) + (hval << 8) + (hval << 24);
         }
-        if(asString){
-            // Convert to 8 digit hex string
+        if(asString)
             return ("0000000" + (hval >>> 0).toString(16)).substr(-8);
-        }
         return hval >>> 0;
     };
     #getColor(str){
