66dfd14da4e71784aa92bca9807921ac689b3db7
Nils Köpke <68548675+nkoepke@users.noreply.github.com>
2022-10-21 11:55:40 +0200

    Update profile-image.rb

--- src/server-variants/profile-image.rb
+++ src/server-variants/profile-image.rb
@@ -10,7 +10,7 @@ class ProfileImage
         @fontWeight = options["fontWeight"] || "normal"
     end
     def svg # returns svg as string
-        return "<svg xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' viewBox='0 0 100 100' width='100' height='100' style='font-weight: #{@fontWeight};'><rect width='100' height='100' x='0' y='0' fill='#{@backgroundColor}'></rect><text x='50%' y='53%' alignment-baseline='central' text-anchor='middle' font-family='#{@fontFamily}' font-size='#{@fontSize}' fill='#{@textColor}' dominant-baseline='middle'>#{shortName()}</text></svg>"
+        return "<svg xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' viewBox='0 0 100 100' width='100' height='100' style='font-weight: #{@fontWeight};'><rect width='100' height='100' x='0' y='0' fill='#{@backgroundColor}'></rect><text x='50%' y='50%' alignment-baseline='central' text-anchor='middle' font-family='#{@fontFamily}' font-size='#{@fontSize}' fill='#{@textColor}' dominant-baseline='middle'>#{shortName()}</text></svg>"
     end
     def shortName # internal function, shortens the name to first two characters.
         return @name.split(" ").map{|word| word[0]}.join("")[0..1].upcase
